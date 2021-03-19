f6404015595048a57acfa9b257bfc7b57258053f
anderskloborg <anderskloborg@gmail.com>
2021-03-19 22:32:33 +0100

    Memoized getCommandParts function

--- src/utils/messageHelpers.ts
+++ src/utils/messageHelpers.ts
@@ -1,5 +1,23 @@
-import { prefix } from "../config";
+import { prefix, commandPartsCacheSize } from "../config";
 
-export function getCommandParts(commandContent: string) {
+export function splitCommandContent(commandContent: string) {
   return commandContent.replace(prefix, "").trimStart().split(" ");
 }
+
+function getCommandPartsMemoized() {
+  let cache: { [key: string]: string[] } = {};
+
+  return (commandContent: string) => {
+    if (!cache[commandContent]) {
+      //Clear cache when we reach a certain limit to not end up keeping too many arrays in memory
+      if (Object.keys(cache).length >= commandPartsCacheSize) {
+        cache = {};
+      }
+      cache[commandContent] = splitCommandContent(commandContent);
+    }
+
+    return cache[commandContent];
+  };
+}
+
+export const getCommandParts = getCommandPartsMemoized();
