8ff45ec215da0999c88e57ce9b1463f04ac0ec63
anderskloborg <anderskloborg@gmail.com>
2023-03-27 18:48:45 +0200

    no longer keep sensitive information in config.ts, remove from gitignore

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const soundsDirPath = "/app/data/sounds";
+export const serverSettingsDirPath = "/app/data/serverSettings";
+export const usageMetricsDirPath = "/app/data/usageMetrics";
+export const prefix = "¤";
+export const leaveTimeoutInMilliseconds = 600000;
+export const commandPartsCacheSize = 200;
+export const maxFileSizeInBytes = 5242880; //5 MiB
