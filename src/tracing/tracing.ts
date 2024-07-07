bca2441cc5dc0deb5f98828b622ab1b9fd30eb5a
anderskloborg <anderskloborg@gmail.com>
2024-07-07 17:40:04 +0200

    Installs typescript as dev dependency.

--- src/tracing/tracing.ts
+++ src/tracing/tracing.ts
@@ -3,7 +3,7 @@ import { HoneycombSDK } from "@honeycombio/opentelemetry-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 
-export const tracingSdk: () => NodeSDK = () => {
+export const tracingSdk = () => {
   if (process.env.HONEYCOMB_API_KEY) {
     return new HoneycombSDK({
       instrumentations: [],
